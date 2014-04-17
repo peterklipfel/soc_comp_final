1397a414ed42a6cc95a3d93f56bd46c0bbe1699b
Peter Klipfel <peter@klipfel.me>
2014-04-17 17:25:14 -0600

    drawing word frequency as a network

--- cooccurrence.py
+++ cooccurrence.py
@@ -3,6 +3,7 @@ import operator
 from itertools import izip
 from collections import defaultdict
 
+import networkx as nx
 import numpy.numarray as na
 
 import pylab as pl
@@ -49,4 +50,48 @@ def bar_graph():
 
   pl.savefig('cooccurringwords.pdf', bbox_inches='tight', pad_inches = 3.0)
 
-bar_graph()
+# credit: https://www.udacity.com/wiki/creating_network_graphs_with_python
+def network(graph, labels=None, graph_layout='shell',
+            node_size=1600, node_color='blue', node_alpha=0.3,
+            node_text_size=12,
+            edge_color='blue', edge_alpha=0.3, edge_tickness=1,
+            edge_text_pos=0.3,
+            text_font='sans-serif'):
+  # create network
+  G=nx.Graph()
+
+  # add edges
+  for edge in graph:
+      G.add_edge(edge[0], edge[1])
+
+  # these are different layouts for the network you may try
+  # shell seems to work best
+  if graph_layout == 'spring':
+      graph_pos=nx.spring_layout(G)
+  elif graph_layout == 'spectral':
+      graph_pos=nx.spectral_layout(G)
+  elif graph_layout == 'random':
+      graph_pos=nx.random_layout(G)
+  else:
+      graph_pos=nx.shell_layout(G)
+
+  # draw graph
+  nx.draw_networkx_nodes(G,graph_pos,node_size=node_size, 
+                         alpha=node_alpha, node_color=node_color)
+  nx.draw_networkx_edges(G,graph_pos,width=edge_tickness,
+                         alpha=edge_alpha,edge_color=edge_color)
+  nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size,
+                          font_family=text_font)
+
+  if labels is None:
+      labels = range(len(graph))
+
+  edge_labels = dict(zip(graph, labels))
+  nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels, 
+                               label_pos=edge_text_pos)
+
+  # show graph
+  pl.savefig('cooccurringwords.pdf')
+
+network(get_order_preserving_list())
+# bar_graph()
