0f66166688550ff792d126ce7f5c734a337152e2
Michael <modberts@gmail.com>
2014-04-23 10:26:59 -0600

    Updated json to csv code
    Fixed NoRetweets.py so it removes retweets correctly. Removed new lines
    from both so the csv format is maintained.

--- Json to csv - NoRetweets.py
+++ Json to csv - NoRetweets.py
@@ -2,9 +2,8 @@
 #
 # Code takes twitter json data and outputs the time and date of a tweet and the tweet text
 # The output format is CSV
+# All new line and return characters are removed from the tweet text
 #
-# "BUGS": retweet_count only returns 0. A tweet is considered a retweet if it starts with RT
-#         This should be fixed to be more accurate based on json Data
 
 import csv
 import json
@@ -17,21 +16,31 @@ input = open("earthquake_large.json")
 No_Retweets = open("No_Retweets.csv",'wb')
 tweetWriter = csv.writer(No_Retweets)
 line = input.readline()
+#write the column headers
 tweetWriter.writerow(["dateTime","tweetText"])
-while (line) : 
-        lineObj = simplejson.loads(line)
-        #result = lineObj.get('result')
-        
-        dateTime = lineObj.get('created_at')
-        retweet = lineObj.get("retweet_count")
-        tweetText = lineObj.get('text')
-        tweetText = tweetText.encode("ascii","ignore")
-        if tweetText[0] == 'R' and tweetText[1] == 'T':
-                retweet = "1"
 
-        if retweet == '0':
+#loop over each line of the json file
+while (line) : 
+        #read in the json object
+        lineObj = simplejson.loads(line)
+
+        #extract data from the json object
+        dateTime = lineObj.get('created_at')
+        tweetText = lineObj.get('text')
+        tweetText = tweetText.encode("ascii","ignore").replace('\n',' ').replace('\r',' ')
+        
+        #If retweeted_Status exists in the json object, it is a retweet
+        is_retweet = 0
+        if "retweeted_status" in lineObj :
+                is_retweet = 1
+        
+        #If the tweet is not a retweet, output to csv
+        if is_retweet == 0:
                 tweetWriter.writerow([dateTime,tweetText])  
+
+        #Advance to the next json line
         line = input.readline()
 
+#Close csv file
 No_Retweets.close()
 
