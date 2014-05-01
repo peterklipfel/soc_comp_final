f9aac575e0d8f754ead1aeb0dd932b9b1b0961cc
Peter Klipfel <peter@klipfel.me>
2014-05-01 14:18:50 -0600

    binning words and taking full hash

--- tweet_word_classifier.py
+++ tweet_word_classifier.py
@@ -5,18 +5,25 @@ import re
 from collections import defaultdict
 
 PRINT_PROGRESS = True
-PRINT_UNRECOGNIZED_TWEETS = True
+PRINT_UNRECOGNIZED_TWEETS = False
 BIN_TWEETS = True
 TWEET_BIN_SIZE = 10000
 TWEET_LIMIT = 40000
+DUMP_DICTIONARY = True
+DUMP_BINS = True
+DICTIONARY_FILE = "wordDict"
+BIN_FILE = "wordBins"
 
 # nouns = set()
 nouns = {}
 nouns = defaultdict(lambda: 0, nouns)
 
+allNouns = {}
+allNouns = defaultdict(lambda: 0, allNouns)
+
 nounsBins = []
 
-disaster_words = ["earthquake", "tsunami", "sismo"]
+disaster_words = ["earthquake", "terremoto", "sismo"]
 regex = re.compile("\d")
 regex2 = re.compile("(.)\1{3,}")
 
@@ -41,18 +48,23 @@ def first(l):
   else:
     return None
 
+def print_progress(i):
+  if PRINT_PROGRESS and i%100 == 0:
+    sys.stdout.write("\rParsed tweets: %i" % i)
+    sys.stdout.flush()
+  
 
 with open('tweets/No_Retweets.csv', 'rb') as csvfile:
   reader = csv.reader(csvfile)
   i = 0
   for row in reader:
+    # end bin if it's reached the TWEET_BIN_SIZE
     if BIN_TWEETS and (i % TWEET_BIN_SIZE == TWEET_BIN_SIZE-1):
       split_list()
     if i > TWEET_LIMIT:
       break
-    if PRINT_PROGRESS and i%100 == 0:
-      sys.stdout.write("\rParsed tweets: %i" % i)
-      sys.stdout.flush()
+    print_progress(i)
+
     text = nltk.word_tokenize(row[1])
     tagged = nltk.pos_tag(text)
 
@@ -68,23 +80,29 @@ with open('tweets/No_Retweets.csv', 'rb') as csvfile:
         disasterpairs.append(x[0])
         print_tweet = False
 
-    if print_tweet:
+    if PRINT_UNRECOGNIZED_TWEETS and print_tweet:
       print row[1]
 
     # data type is [("word", "NN"), ("Bob", "NNP")]
     # use "NNP" to find all proper nouns
     current_nouns = filter(lambda x: x[1].startswith('NNP'), disasterpairs)
-    # current_nouns = filter(lambda x: x[1].startswith('NN'), tagged)
 
     #               get the first element of all tuples in array
     for noun in [j[0] for j in current_nouns]:
       if isNotGarbage(noun):
+        allNouns[noun.lower()] = allNouns[noun.lower()] + 1
         nouns[noun.lower()] = nouns[noun.lower()] + 1
 
     i = i+1
 
 # print nounsBins
-print set(nounsBins[0]).union(set(nounsBins[1])).union(set(nounsBins[2])).union(set(nounsBins[3]))
-# f = open('propernouns', 'w')
-# f.write(repr(nouns))
-# f.close()
+# print set(nounsBins[0]).union(set(nounsBins[1])).union(set(nounsBins[2])).union(set(nounsBins[3]))
+if DUMP_BINS:
+  f = open(BIN_FILE, 'w')
+  f.write(repr(nounsBins))
+  f.close()
+
+if DUMP_DICTIONARY:
+  f = open(DICTIONARY_FILE, 'w')
+  f.write(repr(allNouns))
+  f.close()
